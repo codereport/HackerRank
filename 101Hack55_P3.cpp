c59d90e19fd19b9eb2e1a03cba931c4fbe6d5eae
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-07-10 09:06:07 -0400

    Update 101Hack55_P3.cpp

--- 101Hack55_P3.cpp
+++ 101Hack55_P3.cpp
@@ -1,6 +1,8 @@
 // code_report Solution
 // 
 
+// Original solution
+
 long fewestTowers (vector<int> xs, vector<int> ys) 
 {
    map<int, set<int>> xy;
@@ -32,4 +34,55 @@ long fewestTowers (vector<int> xs, vector<int> ys)
    for (auto i = 0; i < c; ++i) ans += t[i] - b[i] + 1; // could be transform
 
    return ans;
+}
+
+// Modern solution
+
+
+template <typename I, typename P, typename F>
+// requires ForwardIterator<I> &&
+//          StrictWeakOrdering<P> &&
+//          RangeFunction<F>
+F group_equal (I f, I l, P p, F action) {
+   while (f != l) {
+      I cur_end = std::upper_bound (f, l, *f, p);
+      action (f, cur_end);
+      f = cur_end;
+   }
+   return action;
+}
+
+template <typename P>
+void set_perimeter (std::vector<int> &v, P p) {
+   auto limit = *min_element (v.begin (), v.end (), p);
+   for (int i = 1;             v[i - 1] != limit; ++i) if (p (v[i - 1], v[i])) v[i] = v[i - 1];
+   for (int i = v.size () - 2; v[i + 1] != limit; --i) if (p (v[i + 1], v[i])) v[i] = v[i + 1];
+}
+
+template <typename I, typename P>
+void set_perimeter2 (I f, I l, P p) {
+   ++f, --(--l);
+   auto limit = *min_element (v.begin (), v.end (), p);
+   while (*f != limit) v[i] = p (v[i - 1], v[i]) ? v[i - 1] : v[i], ++f;
+   while (*l != limit) v[i] = p (v[i + 1], v[i]) ? v[i + 1] : v[i], --l;
+}
+
+long fewestTowers2 (std::vector<int> xs, std::vector<int> ys)
+{
+   std::vector<std::pair<int, int>> dots (xs.size ());
+   std::transform (xs.begin (), xs.end (), ys.begin (), dots.begin (), [](int x, int y) { return std::make_pair (x, y); });
+   std::sort (dots.begin (), dots.end ());
+
+   std::vector<int> t, b; // top, bottom
+   group_equal (dots.begin (), dots.end (), 
+      [](const auto& dot1, const auto& dot2) { return dot1.first < dot2.first; },
+      [&](auto f, auto l) mutable { b.push_back (f->second); t.push_back ((--l)->second); });
+
+   set_perimeter (b, std::less<>    ());
+   set_perimeter (t, std::greater<> ());
+
+   std::vector<int> diff (t.size ());
+   std::transform (t.begin (), t.end (), b.begin (), diff.begin (), std::minus<> ());
+   
+   return std::accumulate (diff.begin (), diff.end (), 0) + t.size () - xs.size ();
 }
