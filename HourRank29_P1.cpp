c90e2f06412fb0dd0585bfcc26f85f53175d22e6
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-08-02 12:20:07 -0400

    Create HourRank29_P1.cpp

---
+++ HourRank29_P1.cpp
@@ -0,0 +1,18 @@
+// code_report Solution
+// https://youtu.be/Ev5V2d_fubU
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+#define FORI(s,n) for(int i = s; i < n; i++)
+#define FORJ(s,n) for(int j = s; j < n; j++)
+
+string solve (vector<vector<int>> b) 
+{
+   int n = b.size ();
+   FORI (0, n) FORJ (1, n) if (b[i][j] == b[i][j - 1]) return "No";
+   FORI (0, n) FORJ (1, n) if (b[j][i] == b[j - 1][i]) return "No";
+   return "Yes";
+}
