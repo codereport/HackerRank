83e273cac1635230b6a580724b5a92c78f822531
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-09-09 12:22:35 -0700

    Update Uni_CodeSprint_5_P3.cpp

--- Uni_CodeSprint_5_P3.cpp
+++ Uni_CodeSprint_5_P3.cpp
@@ -12,34 +12,51 @@ int dfs (const vvi& al, vi& c, int i, int group) {
    return group;
 }
 
-int solve (int n, vvi edges, int a, int b) {
-   vvi al (n); // adjacency list
+auto create_adjacency_list (int n, const vvi &edges) {
+   vvi al (n);
    for (const auto& e : edges) {
       int x = e[0] - 1, y = e[1] - 1;
       al[x].push_back (y);
       al[y].push_back (x);
    }
+   return al;
+}
 
+auto determine_components (int n, const vvi &al) {
+   vi c (n, -1);
+   for (int i = 0; i < n; ++i) 
+      int j = dfs (al, c, i, i);
+   return c;
+}
+
+auto find_min_max_edges (int n, const vvi &al, const vi &c) {
    unordered_map<int, pair<ll, ll>> m;
 
-   // dfs to find different components
-   vi c (n, -1); // components
-   for (int i = 0; i < n; ++i) {
-      int j = dfs (al, c, i, i);
-      if (j != -1) m[j] = { 0, n };
+   // initialize min/max for each component
+   for (const auto& e : c) {
+      m[e] = { 0, n };
    }
 
    // find min and max edges for each component
    for (int i = 0; i < n; i++) {
-      auto& p  = m[c[i]];
-      p.first  = max (p.first, (ll)al[i].size ());
+      auto& p = m[c[i]];
+      p.first = max (p.first, (ll)al[i].size ());
       p.second = min (p.second, (ll)al[i].size ());
    }
 
-   auto criteria = [&, i = 0] (const auto& t) mutable {
-      auto s = t.size ();    // edges of v
-      auto p = m[c[i]]; i++; // min and max edges (for a node) for the component
-      return (s > a * p.second && s < b * p.first);
+   return m;
+}
+
+int solve (int n, vvi edges, int a, int b)
+{
+   auto al = create_adjacency_list (n, edges);
+   auto c  = determine_components  (n, al);
+   auto m  = find_min_max_edges    (n, al, c);
+
+   auto criteria = [&, i = 0] (const auto& edges) mutable {
+      auto v_e = edges.size ();
+      auto p = m[c[i]]; i++; 
+      return (a * p.second < v_e && v_e < b * p.first);
    };
 
    return count_if (al.begin (), al.end (), criteria);
