0ba49fa24dd71cc95b55b8497401424023b7571f
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-10-03 22:18:55 -0700

    Update Dynamic_Programming_04_Abbrevition.cpp

--- Dynamic_Programming_04_Abbrevition.cpp
+++ Dynamic_Programming_04_Abbrevition.cpp
@@ -1,43 +1,42 @@
 // code_report Solution
 // 
 
+
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+
 using namespace std;
+
+void rec(string A, string B);
 
 unordered_set<string> memo;
 bool possible;
 
-void rec (string A, string B) {
-    if (possible || A.size () < B.size ()) return;
-    if (B.empty ()) {
-        if (all_of(A.begin(), A.end(), [](auto e) { return islower(e); })) 
-            possible = true;
-        return;
-    }
-    
-    if (memo.count (A+'#'+B)) return;
-    memo.insert (A+'#'+B);
-    
-    if (toupper(A[0]) != B[0]) {
-        if (isupper(A[0])) return;
-        A.erase (0,1);    
-        rec (A, B); 
-    } else {
-        if (islower(A[0])) {
-            A.erase (0,1);
-            rec (A, B); 
-        } else {
-            A.erase (0,1); 
-        }
-        B.erase (0,1);
-        rec (A, B);
-    }
-    return;
+auto abbreviation(string a, string b) {
+   possible = false;
+   memo.clear();
+   rec(a, b);
+   return possible ? "YES" : "NO";
 }
 
-// Complete the abbreviation function below.
-string abbreviation(string a, string b) {
-    possible = false;
-    memo.clear ();
-    rec (a, b);
-    return possible ? "YES" : "NO";
+void rec(string A, string B) {
+   if (possible || A.size() < B.size()) return;
+   if (B.empty()) {
+      if (all_of(A.begin(), A.end(), [](auto e) { return islower(e); }))
+         possible = true;
+      return;
+   }
+
+   auto p = memo.insert(A + '#' + B);
+   if (!p.second) return; // return if A#B already in set
+
+   auto fc = A[0]; // first char 
+   A.erase(0, 1);
+   if (islower(fc)) rec(A, B);
+   if (toupper(fc) != B[0]) return;
+   B.erase(0, 1);
+   rec(A, B);
+
+   return;
 }
