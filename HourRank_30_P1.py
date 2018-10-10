ace18bb75b6f9062b63704e9422a652a5412fee7
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-10-10 00:40:29 -0700

    Update HourRank_30_P1.py

--- HourRank_30_P1.py
+++ HourRank_30_P1.py
@@ -1,6 +1,8 @@
 # code_report Solution
 # https://youtu.be/uOG3QyxIjso
 
+# Solution 1:
+
 import defaultdict from collections
 
 def solve(names):
@@ -24,3 +26,26 @@ def solve(names):
             if not inserted: 
                 l.append(name)
     return l
+
+#Solution 2: 
+
+def solve(names):
+    trie = {}
+    res = []
+    for name in names:
+        node = trie
+        printed = False
+        for i, x in enumerate(name, start=1):
+            if x not in node:
+                if not printed: 
+                    res.append(name[:i])
+                    printed = True
+                node[x] = defaultdict(int)
+
+            node = node[x]
+
+        node['count'] += 1
+
+        if not printed: 
+            res.append (name + (' '+str(node['count']) if node['count']!=1 else ''))
+    return res
