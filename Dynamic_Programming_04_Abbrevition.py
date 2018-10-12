87d15a7f5251f4a027db7f531be2f4b111f29c0d
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-10-11 22:07:19 -0700

    Update Dynamic_Programming_04_Abbrevition.py

--- Dynamic_Programming_04_Abbrevition.py
+++ Dynamic_Programming_04_Abbrevition.py
@@ -1,4 +1,6 @@
 # code_report Solution
+
+# Solution 1:
 # https://youtu.be/DW92IHf8KK8
 
 memo = set()
@@ -35,3 +37,19 @@ def rec(a, b):
     rec(a,b)
     
     return
+
+# Solution 2:
+# https://youtu.be/ViILdd8495M
+
+def abbreviation(a, b):
+    n, m = len(a), len(b)
+    dp = [[0 for i in range(m+1)] for j in range(n+1)]
+    dp[0][0] = 1
+    
+    for i in range(n):
+        for j in range(m+1):
+            if not dp[i][j]: continue;
+            if j < m and a[i].upper() == b[j]: dp[i+1][j+1] = 1;
+            if not a[i].isupper():             dp[i+1][j  ] = 1;
+    
+    return 'YES' if dp[n][m] else 'NO'
