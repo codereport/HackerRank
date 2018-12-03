6a8471118e3cd7482497625dd1f67e19f839b5af
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-12-02 18:07:50 -0800

    Create HourRank_31_P1.cpp

---
+++ HourRank_31_P1.cpp
@@ -0,0 +1,9 @@
+// code_report Solution
+// https://youtu.be/XKQQYCScP0U
+
+int solve(int h, vector<int> w, vector<int> l) {
+   int p = 0;
+   for (int i = 0; i < w.size(); ++i)
+      p = max(p, w[i] - l[i] / 4);
+   return max(0, p - h);
+}
