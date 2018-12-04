cc97dc8cc87135d97de433a3d73f8e8c0f7f75e4
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-12-03 22:23:52 -0800

    Update HourRank_31_P2.py

--- HourRank_31_P2.py
+++ HourRank_31_P2.py
@@ -5,8 +5,7 @@ def avg(a, b):
     return (sum(a)+sum(b))/len(a)
 
 def solve(n, a):
-    a   = sorted(a, reverse = True)
-    on  = a[0:n]
-    off = a[n:]
+    a = sorted(a, reverse = True)
+    on, off = a[0:n], a[n:]
     while len(on) > 0 and avg(on, off) < max(on): on.pop(0)
     print("%.5f"% (avg(on, off)))
