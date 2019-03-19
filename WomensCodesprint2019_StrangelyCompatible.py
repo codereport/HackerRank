f6a12a0e64e73bef5f839d71bd2acad5af3aa4a4
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-03-18 19:54:49 -0700

    Create WomensCodesprint2019_StrangelyCompatible.py

---
+++ WomensCodesprint2019_StrangelyCompatible.py
@@ -0,0 +1,16 @@
+# code_report Solution
+# Video Link:
+# Problem Link: https://www.hackerrank.com/contests/hackerrank-all-womens-codesprint-2019/challenges/strangely-compatible
+
+def pair_count(t):
+  return sum((i*(i-1))//2 for i in t.values())
+
+def strangelyCompatible(students, m):
+  changed, same = {}, {}
+  for s in students:
+    same[s] = same.get(s, 0) + 1
+    for i in range(m):
+      t = s[:i] + '!' + s[i+1:]
+      changed[t] = changed.get(t, 0) + 1
+
+  return pair_count(changed) - pair_count(same) * m
